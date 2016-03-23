a87ee79627ccd34a1d2a796257073c1e2daaca5b
Igor Vasilenko <ivasilenko@yotateam.com>
2016-03-23 10:59:40 +0300

    Added NSString+MD5 category

---
+++ Marvel/Marvel/Utils/Categories/NSString+MD5.h
@@ -0,0 +1,12 @@
+//
+// Created by Igor Vasilenko on 23/03/16.
+// Copyright (c) 2016 Igor Vasilenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (MD5)
+
+- (NSString *)MD5String;
+
+@end
